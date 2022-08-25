da61d31f71d88ee37f6e0d5b13c09d0823c609a4
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-25 23:07:39 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const hqBlock=42
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    someValue=43
+
+    return `The distance from the Hq is ${someValue-hqBlock} blocks`
+  }
+  function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    return 
+
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  function distanceTravelledInFeet(start, destination) {
+    if (distance<=400){
+      return ('This one is on me!')}
+     
+      else if (distance>2000 && distance<=2500){
+    
+       return 'I will gladly take your thirty bucks.'
+      }
+      else {
+        return 'No can do.';
+    
+      }
+    return `The distance travelleved is ${(destination-start)*264} feet`
+    //returns the number of feet traveled
+  }
+  function calculatesFarePrice(start, destination) {
+    return `The fare for the distance travelled is ${ (destination-start )*}`
+    //returns the fare for the customer
+  }
