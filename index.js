41945ad6e060f7d28116843f46f8cb54e2a7d59d
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-29 13:11:49 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 // Code your solution in this file!
-const hqBlock=42
-function distanceFromHqInBlocks(someValue) {
-    //returns the number of blocks given a value
-    someValue=43
+function distanceFromHqInBlocks(distance) {
+  return Math.abs(distance - 42);
+}
 
-    
-  }
-  function distanceFromHqInFeet(someValue) {
-    distanceFromHqInBlocks(someValue);
-    
-    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
-    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+function distanceFromHqInFeet(distance) {
+      return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+      return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+      var distance = distanceTravelledInFeet(start, destination);
+      if (distance>2500) {
+            return 'cannot travel that far';
+      }
+      if (distance < 400) {
+        return 0;
+      } else if (distance>400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+      } else if (distance>2000 && distance<2500){
+        return 25;
+      }  else {
+        return "can not travel that far"
+      }
+}
+
   
-    // the return value of distanceFromHqInBlocks can then be used to calculate feet
-  }
-  return `The distance from the Hq is ${someValue-hqBlock} blocks`
+//     // the return value of distanceFromHqInBlocks can then be used to calculate feet
+//   }
+//   return `The distance from the Hq is ${someValue-hqBlock} blocks`
 
-  function distanceTravelledInFeet(start, destination) {
-    if (distance<=400){
-      return ('This one is on me!')}
+//   function distanceTravelledInFeet(start, destination) {
+//     if (distance<=400){
+//       return ('This one is on me!')}
      
-      else if (distance>2000 && distance<=2500){
+//       else if (distance>2000 && distance<=2500){
     
-       return 'I will gladly take your thirty bucks.'
-      }
-      else {
-        return 'No can do.';
+//        return 'I will gladly take your thirty bucks.'
+//       }
+//       else {
+//         return 'No can do.';
     
-      }
-    return `The distance travelleved is ${(destination-start)*264} feet`
-    //returns the number of feet traveled
-  }
-  function calculatesFarePrice(start=34, destination=38) {
-    
-    //returns the fare for the customer
-  }
-  return `The fare for the distance travelled is ${ (destination-start )*264}`
+//       }
+//     return `The distance travelleved is ${(destination-start)*264} feet`
+//     //returns the number of feet traveled
+//   }
